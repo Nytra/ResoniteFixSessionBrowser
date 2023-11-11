aa81966485bc8ed5de5a1b7b0167da98c1884f49
Nytra <samueltscott@gmail.com>
2023-11-11 03:55:58 +0000

    update assemblyinfo

--- FixSessionBrowser/Properties/AssemblyInfo.cs
+++ FixSessionBrowser/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModNameHere")]
+[assembly: AssemblyTitle("FixSessionBrowser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nytra")]
-[assembly: AssemblyProduct("ModNameHere")]
+[assembly: AssemblyProduct("FixSessionBrowser")]
 [assembly: AssemblyCopyright("Copyright © Nytra 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
